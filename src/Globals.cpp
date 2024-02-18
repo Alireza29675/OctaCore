8e5d7408ebd36d3d39bfce2060cfecc5ba8a12cb
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2024-02-18 20:14:59 +0100

    Update build_flags and WiFiModule initialization

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -1,6 +1,6 @@
 #include "Globals.h"
 
-WiFiModule wifiModule(ACCESS_POINT_SSID, ACCESS_POINT_PASSWORD);
+WiFiModule wifiModule("ACCESS_POINT_SSID", "ACCESS_POINT_PASSWORD");
 WebSocketModule webSocketModule(81);
 LEDStripController ledStrip;
 ServoController servoMotor(SERVO_DATA_PIN, 0);
